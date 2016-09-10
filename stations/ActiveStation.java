80d4cc2413298e96c3fc26a509162adb9737b4f9
DanMoriarty <d.moriarty@hotmail.com.au>
2016-09-10 22:37:33 +1000

    Incorporate PassngerGenerator interface

--- stations/ActiveStation.java
+++ stations/ActiveStation.java
@@ -2,28 +2,30 @@ package com.unimelb.swen30006.metromadness.stations;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Random;
 
 import com.unimelb.swen30006.metromadness.exceptions.PlatformCapacityException;
 import com.unimelb.swen30006.metromadness.exceptions.TrainCapacityException;
 import com.unimelb.swen30006.metromadness.passengers.Passenger;
 import com.unimelb.swen30006.metromadness.passengers.PassengerGenerator;
 import com.unimelb.swen30006.metromadness.routers.PassengerRouter;
+import com.unimelb.swen30006.metromadness.tracks.Line;
 import com.unimelb.swen30006.metromadness.trains.Train;
 
 /**
  * A station that is active and thus able to take passengers from any
  * arriving trains
  */
-public class ActiveStation extends Station {
+public class ActiveStation extends Station implements PassengerGenerator {
 
-	public PassengerGenerator g;
+	
 	public ArrayList<Passenger> waiting;
 	public float maxVolume;
 	
 	public ActiveStation(float x, float y, PassengerRouter router, String name, float maxPax) {
 		super(x, y, router, name);
 		this.waiting = new ArrayList<Passenger>();
-		this.g = new PassengerGenerator(this, this.lines, maxPax);
+		
 		this.maxVolume = maxPax;
 	}
 	
@@ -55,7 +57,7 @@ public class ActiveStation extends Station {
 				return;
 			}
 			// Add the new passenger
-			Passenger[] ps = this.g.generatePassengers();
+			Passenger[] ps = this.generatePassengers();
 			for(Passenger p: ps){
 				try {
 					t.embark(p);
@@ -67,6 +69,40 @@ public class ActiveStation extends Station {
 				}
 			}
 		}
+	}
+
+	@Override
+	public Passenger generatePassenger() {
+		// Pick a random station from the line
+		Line line = this.lines.get((int)Math.random()*(this.lines.size()-1));
+		int current_station = line.stations.indexOf(this);
+		boolean forward = Math.random()>0.5f;
+				
+		// If we are the end of the line then set our direction forward or backward
+		forward = line.endOfLine(current_station, forward);
+			
+		// Find the destination station
+		int index = 0;
+		Random random = new Random();
+		if (forward){
+			index = random.nextInt(line.stations.size() -1-current_station) + current_station + 1;
+		}else {
+			index = current_station - 1 - random.nextInt(current_station);
+		}
+		Station s = line.stations.get(index);
+				
+		return new Passenger(this, s);
+	}
+
+
+	@Override
+	public Passenger[] generatePassengers() {
+		int count = (int) (Math.random()*maxVolume);
+		Passenger[] passengers = new Passenger[count];
+		for(int i=0; i<count; i++){
+			passengers[i] = generatePassenger();
+		}
+		return passengers;
 	}
 
 }
